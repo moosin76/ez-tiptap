3b553996651a1d0a3ceb363daf13e247fd10bc84
skadiD <kazdel@qq.com>
2022-01-22 22:02:40 +0800

    upgrade tiptap core

--- src/tiptap/extension/CustomTaskItem.js
+++ src/tiptap/extension/CustomTaskItem.js
@@ -1,87 +1,84 @@
-import { Node, mergeAttributes } from '@tiptap/core';
-import { wrappingInputRule } from 'prosemirror-inputrules';
-import { VueNodeViewRenderer } from "@tiptap/vue-2";
+import {Node, mergeAttributes, wrappingInputRule} from '@tiptap/core';
+import {VueNodeViewRenderer} from "@tiptap/vue-2";
 import TaskItemComponent from '../view/TaskItemComponent.vue';
 
 const inputRegex = /^\s*(\[([ |x])\])\s$/
-
 const TaskItem = Node.create({
-	name: 'taskItem',
+    name: 'taskItem',
 
-  defaultOptions: {
-    nested: true,
-    HTMLAttributes: {},
-  },
+    addOptions: {
+        nested: true,
+        HTMLAttributes: {},
+    },
 
-  content() {
-    return this.options.nested ? 'paragraph block*' : 'paragraph+'
-  },
+    content() {
+        return this.options.nested ? 'paragraph block*' : 'paragraph+'
+    },
+    defining: true,
 
-	defining: true,
+    addAttributes() {
+        return {
+            checked: {
+                default: false,
+                parseHTML: element => ({
+                    checked: element.getAttribute('data-checked') === 'true',
+                }),
+                renderHTML: attributes => ({
+                    'data-checked': attributes.checked,
+                }),
+                keepOnSplit: false,
+            },
+        }
+    },
 
-  addAttributes() {
-    return {
-      checked: {
-        default: false,
-        parseHTML: element => ({
-          checked: element.getAttribute('data-checked') === 'true',
-        }),
-        renderHTML: attributes => ({
-          'data-checked': attributes.checked,
-        }),
-        keepOnSplit: false,
-      },
-    }
-  },
+    parseHTML() {
+        return [
+            {
+                tag: 'li[data-type="taskItem"]',
+                priority: 51,
+            },
+        ]
+    },
 
-	parseHTML() {
-    return [
-      {
-        tag: 'li[data-type="taskItem"]',
-        priority: 51,
-      },
-    ]
-  },
+    renderHTML({HTMLAttributes}) {
+        return ['li', mergeAttributes(
+            this.options.HTMLAttributes,
+            HTMLAttributes,
+            {'data-type': 'taskItem'},
+        ), 0]
+    },
 
-  renderHTML({ HTMLAttributes }) {
-    return ['li', mergeAttributes(
-      this.options.HTMLAttributes,
-      HTMLAttributes,
-      { 'data-type': 'taskItem' },
-    ), 0]
-  },
+    addKeyboardShortcuts() {
+        const shortcuts = {
+            Enter: () => this.editor.commands.splitListItem('taskItem'),
+            'Shift-Tab': () => this.editor.commands.liftListItem('taskItem'),
+        }
 
-	addKeyboardShortcuts() {
-    const shortcuts = {
-      Enter: () => this.editor.commands.splitListItem('taskItem'),
-      'Shift-Tab': () => this.editor.commands.liftListItem('taskItem'),
-    }
+        if (!this.options.nested) {
+            return shortcuts
+        }
 
-    if (!this.options.nested) {
-      return shortcuts
-    }
+        return {
+            ...shortcuts,
+            Tab: () => this.editor.commands.sinkListItem('taskItem'),
+        }
+    },
 
-    return {
-      ...shortcuts,
-      Tab: () => this.editor.commands.sinkListItem('taskItem'),
-    }
-  },
+    addInputRules() {
+        return [
+            wrappingInputRule({
+                find: inputRegex,
+                type: this.type,
+                getAttributes: match => ({
+                    checked: match[match.length - 1] === 'x',
+                })
+            }),
+        ]
+    },
 
-	addInputRules() {
-    return [
-      wrappingInputRule(
-        inputRegex,
-        this.type,
-        match => ({
-          checked: match[match.length - 1] === 'x',
-        }),
-      ),
-    ]
-  },
-
-	addNodeView() {
-		return VueNodeViewRenderer(TaskItemComponent);
-	},
+    addNodeView() {
+        return VueNodeViewRenderer(TaskItemComponent);
+    },
 })
 
 export default TaskItem;
